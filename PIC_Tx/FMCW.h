b7bf86ac18a7f7fe00dbbcf7969eddf5dc992399
MadokaKurosaki <kuroyagimaa@gmail.com>
2018-09-03 17:32:45 +0900

    add function to send morse / merge CW_branch / "changeCharMorse " does not finish

--- PIC_Tx/FMCW.h
+++ PIC_Tx/FMCW.h
@@ -18,6 +18,8 @@ void downlinkReceivedCommand(UBYTE, UBYTE, UBYTE, UBYTE);
 void downlinkFMSignal(UBYTE,UBYTE, UBYTE, UBYTE, UBYTE);
 
 void Morse_V(void);
+int changeCharMorse (char);
+void sendMorse(char*);
 
 #endif	/* FMCW_H */
 
