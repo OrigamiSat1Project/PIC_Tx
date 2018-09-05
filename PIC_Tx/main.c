e46c1efbb5e092b78aac772085a190f94059c18a
MadokaKurosaki <kuroyagimaa@gmail.com>
2018-09-05 10:51:32 +0900

    only morse_V in main.c (no intterrupt)

--- PIC_Tx/main.c
+++ PIC_Tx/main.c
@@ -69,62 +69,62 @@ void interrupt InterReceiver(void);
 //}
 
 
-void interrupt InterReceiver(void){
-    int commandSize;
-    commandSize = 10;
-    UBYTE RXDATA[10];//array size = commandSize
-//    UBYTE RXDATA[COMMAND_SIZE];
-//    volatile static int intr_counter;
-    if (RCIF == 1) {
-        for (int i = 0; i < 10; i++){
-            RXDATA[i] = getChar();
+//void interrupt InterReceiver(void){
+//    int commandSize;
+//    commandSize = 10;
+//    UBYTE RXDATA[10];//array size = commandSize
+////    UBYTE RXDATA[COMMAND_SIZE];
+////    volatile static int intr_counter;
+//    if (RCIF == 1) {
+//        for (int i = 0; i < 10; i++){
+//            RXDATA[i] = getChar();
+////            putChar(RXDATA[i]);
+//        }
+//        for (int i = 0; i < 10; i++){
 //            putChar(RXDATA[i]);
-        }
-        for (int i = 0; i < 10; i++){
-            putChar(RXDATA[i]);
-            NOP();
-        }
-       //TODO add case RXDATA[0]!=t or g
-        UWORD crcResult, crcValue;
-        UBYTE crcResultHigh,crcResultLow,crcValueHigh,crcValueLow;
-        crcResult = crc16(0,RXDATA,8);
-        crcValue =  CRC_check(RXDATA,8);
-        crcResultHigh = crcResult>>8;
-        crcResultLow = crcResult & 0x00FF;
-        crcValueHigh = crcValue>>8;
-        crcValueLow = crcValue & 0x00FF;
-        
-        putChar(crcResultHigh);
-        putChar(crcResultLow);
-        putChar(crcValueHigh);
-        putChar(crcValueLow);
-        
-        if(crcResult == crcValue){
-            putChar('C');
-            switch(RXDATA[1]){
-                case 0x75:
-                    downlinkReceivedCommand(RXDATA[2],RXDATA[3],RXDATA[4],RXDATA[5]);
-                    break;
-                case 0x63:
-                    //CwDownLink(RXDATA[2],RXDATA[3],RXDATA[4],RXDATA[5],RXDATA[6]);
-                    CwDownLinkForTest();  //CW about10seconds
-                    break;
-                case 0x66:
-                    //downlinkFMSignal(RXDATA[2],RXDATA[3],RXDATA[4],RXDATA[5],RXDATA[6]);
-                    FmDownLinkForTest();
-                    break;
-                case 0x61:
-                    cutWire(RXDATA[2],RXDATA[3]);
-                    break;
-            }
-        }else{
-            
-            putChar('D');
-            ///コマンドCRCダメだった時の処理
-        }
-        RCIF = 0;
-    }
-}
+//            NOP();
+//        }
+//       //TODO add case RXDATA[0]!=t or g
+//        UWORD crcResult, crcValue;
+//        UBYTE crcResultHigh,crcResultLow,crcValueHigh,crcValueLow;
+//        crcResult = crc16(0,RXDATA,8);
+//        crcValue =  CRC_check(RXDATA,8);
+//        crcResultHigh = crcResult>>8;
+//        crcResultLow = crcResult & 0x00FF;
+//        crcValueHigh = crcValue>>8;
+//        crcValueLow = crcValue & 0x00FF;
+//        
+//        putChar(crcResultHigh);
+//        putChar(crcResultLow);
+//        putChar(crcValueHigh);
+//        putChar(crcValueLow);
+//        
+//        if(crcResult == crcValue){
+//            putChar('C');
+//            switch(RXDATA[1]){
+//                case 0x75:
+//                    downlinkReceivedCommand(RXDATA[2],RXDATA[3],RXDATA[4],RXDATA[5]);
+//                    break;
+//                case 0x63:
+//                    //CwDownLink(RXDATA[2],RXDATA[3],RXDATA[4],RXDATA[5],RXDATA[6]);
+//                    CwDownLinkForTest();  //CW about10seconds
+//                    break;
+//                case 0x66:
+//                    //downlinkFMSignal(RXDATA[2],RXDATA[3],RXDATA[4],RXDATA[5],RXDATA[6]);
+//                    FmDownLinkForTest();
+//                    break;
+//                case 0x61:
+//                    cutWire(RXDATA[2],RXDATA[3]);
+//                    break;
+//            }
+//        }else{
+//            
+//            putChar('D');
+//            ///コマンドCRCダメだった時の処理
+//        }
+//        RCIF = 0;
+//    }
+//}
 
 
 //void interrupt InterReceiver(void){
@@ -207,19 +207,19 @@ void main(void) {
             putch('O');
             __delay_ms(500);
         }*/
-        putChar('H');
-        CWKEY = 0;
-        __delay_ms(2000);
-        FMPTT = 1;
-        __delay_ms(2000);
-        FMPTT = 0;
+//        putChar('H');
+//        CWKEY = 0;
+//        __delay_ms(2000);
+//        FMPTT = 1;
+//        __delay_ms(2000);
+//        FMPTT = 0;
         //TODO check AD value
         
         /*---------------------------------*/
         //TODO send CW command
-        UBYTE CW_READ_DATA[];
-        ReadDataFromEEPROM(EEPROM_address,CW_DOWNLINK_MAIN_HIGH_ADDRESS, CW_DOWNLINK_MAIN_LOW_ADDRESS, *CW_READ_DATA, CW_DOWNLINK_EEPROM_DataLength);
-        FMPTT = low;
+//        UBYTE CW_READ_DATA[];
+//        ReadDataFromEEPROM(EEPROM_address,CW_DOWNLINK_MAIN_HIGH_ADDRESS, CW_DOWNLINK_MAIN_LOW_ADDRESS, *CW_READ_DATA, CW_DOWNLINK_EEPROM_DataLength);
+//        FMPTT = low;
         
         //test1 : not use function 'sendMorse'
         Morse_V();   //send test morse data 'V'
