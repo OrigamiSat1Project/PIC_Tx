0ace3f466f294ae6282494e5fa320d5d3931dc13
BrittaHhm <Britta-Hohmann@t-online.de>
2018-10-01 12:35:22 +0900

    new function resetWDT

--- PIC_Tx/WDT.c
+++ PIC_Tx/WDT.c
@@ -1,6 +1,9 @@
 #include <xc.h>
 #include "WDT.h"
 #include "Type_define.h"
+#include "pinDefine.h"
+
+void resetWDT(void);
 
 void Init_WDT(void){
     INTCONbits.GIE  = 1;    // Grobal interrupt??????
@@ -21,6 +24,28 @@ void Init_WDT(void){
     T1CONbits.TMR1ON = 1;   // Timer1??????
 }
 
+//process command data if the command type is 'WDT' 
+void commandWDT(UBYTE command, UBYTE timeHigh, UBYTE timeLow){   //times are given in ms
+    if(command = 's'){
+        //Command leads to a reset of PIC (instead of "stop send pulse" use "resetWDT", 
+        //this should have same result, but safes the time since it is not necessary
+        //to wait until WDT realises that phase is missing (would take about 11 sec))
+        resetWDT();
+    }else{
+        //TODO: return error message
+    }
+}
+
+//Resets the WDT and as consequence also resets the TX_PIC; LOW==ON HIGH==OFF
+void resetWDT(void){
+    if(WDT_POWER== LOW){
+       WDT_POWER= HIGH;//TODO: check if a delay is needed
+       WDT_POWER= LOW;
+    }else{
+       WDT_POWER= LOW;//TODO: check if a delay is needed
+       WDT_POWER= HIGH;
+    }
+}
 
 //
 //void interrupt intr(void){
